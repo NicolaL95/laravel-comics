10a67622556e604f5d94cd0347dbdad333e05269
NicolaL95 <nicola.lilla@gmail.com>
2021-12-17 19:11:15 +0100

    Partial end styling header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <!-- FontAwesome 5.15.3 CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- (Optional) Use CSS or JS implementation -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" integrity="sha512-RXf+QSDCUQs5uwRKaDoXt55jygZZm2V++WUZduaU/Ui/9EGp3f/2KZVahFZBKGH0s774sd3HmrhUy+SgOFQLVQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <title>Laravel Comics</title>
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,16 @@
 <header id="header_container">
+    <div class="bg_a w-100">
+    <div class="container">
+        <div class="extra_info justify-content-end d-flex">
+            <p>DCPOWER<span>&trade;</span>VISA<span>&reg;</span></p>
+            <p class="d-flex align-items-center">ADDITIONAL DC SITES<i class="fas fa-sort-down waterfall_menu"></i></p>
+        </div>
+        </div>
+         </div>
+        <div class="container">
+        <div class="navbar_control d-flex align-items-center justify-content-between">
     @include('partials.logo')
-    <nav>
+    <nav class="d-flex align-items-center justify-content-between w-100 px-5 fw-bold">
         <a href="">CHARACTER</a>
         <a href="">COMICS</a>
        <a href="">MOVIES</a>
@@ -12,5 +22,9 @@
         <a href="">NEWS</a>
         <a href="">SHOP</a>
     </nav>
-    <input type="text">
+    <input type="text" placeholder="Search">
+        </div>
+        </div>
+        <img class="jumbotron_image" src="{{asset('img/jumbotron.jpg')}}" alt="Teen Titans">
+    </div>
 </header>
