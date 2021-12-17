d95031984d53c71fd89f4947cc21e60e569333f1
NicolaL95 <nicola.lilla@gmail.com>
2021-12-17 21:06:54 +0100

    Add footer icons

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,12 @@
 <footer id='footer_container'>
     <div class="link_container">
+        <div class="container">
+            <div class="row">
+               <div class="col-6 py-5">
+                   <div class="row ">
+                       <div class="col-4">           
         <h4>DC COMICS</h4>
-        <ul>
+        <ul class="footer_text_c">
             <li>Characters</li>
             <li>Comics</li>
             <li>Movies</li>
@@ -11,12 +16,14 @@
             <li>News</li>
         </ul>
          <h4>SHOP</h4>
-        <ul>
+        <ul class="footer_text_c">
             <li>Shop DC</li>
             <li>Shop DC collectibles</li>
         </ul>
+                        </div>
+                        <div class="col-4">  
          <h4>DC</h4>
-        <ul>
+        <ul class="footer_text_c">
             <li>terms Of Use</li>
             <li>Privacy policy(New)</li>
             <li>Ad Choices</li>
@@ -29,22 +36,36 @@
             <li>Shop Help</li>
             <li>Contacs Us</li>
         </ul>
+                        </div>
+                         <div class="col-4">  
          <h4>SITES</h4>
-        <ul>
+        <ul class="footer_text_c">
             <li>DC</li>
             <li>MD Magazine</li>
             <li>DC Kids</li>
             <li>DC Universe</li>
             <li>DC Power Visa</li>
         </ul>
+                        </div>
         <div class="copy_container">
-            <p></p>
+            <p class="footer_text_c">All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <span class="text-primary">noted here</span>. All rights reserved. <span class="text-primary">Cookies Setting</span></p>
+        </div>
+             </div> 
+              </div>
+             <div class="col-6 logo_bg">
+            </div>
+        </div>
         </div>
     </div>
-    <div class="cta_container">
+    <div class="cta_container d-flex justify-content-between align-items-center">
         <button>SIGN-UP NOW!</button>
         <div class="social_container">
         <p>FOLLOW US</p>
+        <i class="fab fa-facebook-f"></i>
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-pinterest-p"></i>
+        <i class="fab fa-pinterest-p"></i>
+        <i class="fas fa-map-marker-alt"></i>
         </div>
     </div>
 </footer>
