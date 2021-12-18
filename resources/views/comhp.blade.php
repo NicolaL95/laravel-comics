5ba93183fe6938cf74867ebe523f980c12bcb5b2
NicolaL95 <nicola.lilla@gmail.com>
2021-12-18 23:25:35 +0100

    Add complete footer style and content main

--- resources/views/comhp.blade.php
+++ resources/views/comhp.blade.php
@@ -13,4 +13,32 @@
 @endforeach
     </div>
 </div>
+ </div>
+      <button class="load">LOAD MORE</button>
+    </div>
+    <div class="generic_bg">
+      <div class="generic_container">
+        <div class="generic_content">
+             <img src="{{asset('img/buy-comics.png')}}" alt="Icona Generica">
+          <img src="../assets/buy-comics-digital-comics.png" alt="" />
+          <p>Digital Comics</p>
+        </div>
+        <div class="generic_content">
+            <img src="{{asset('img/buy-comics--merchandise.png')}}" alt="Icona Generica">
+          <p>DC MERCHANDISE</p>
+        </div>
+        <div class="generic_content">
+            <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="Icona Generica">
+          <p>SUBSCRIPTION</p>
+        </div>
+        <div class="generic_content">
+            <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="Icona Generica">
+          <p>COMIC SHOP LOCATOR</p>
+        </div>
+        <div class="generic_content">
+            <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="Icona Generica">
+          <p>DC POWER VISA</p>
+        </div>
+      </div>
+    </div>
 @endsection
