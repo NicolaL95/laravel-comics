b637b8bb6b3d804f98060b69befe1b29bda56bc7
NicolaL95 <nicola.lilla@gmail.com>
2021-12-20 23:34:59 +0100

    End Style

--- app/Http/Controllers/DCController.php
+++ app/Http/Controllers/DCController.php
@@ -9,6 +9,9 @@ class DCController extends Controller
 public function show($num)
 {
     $dett_com = config('comics');
-    return view('dett_comics',compact('num','dett_com'));
+    $date_format = $dett_com[$num]['sale_date'];
+    $date_format = date("M d y");
+    
+    return view('dett_comics',compact('num','dett_com','date_format'));
 }
 }
--- resources/views/dett_comics.blade.php
+++ resources/views/dett_comics.blade.php
@@ -9,11 +9,11 @@
         </div>
         </div>
     </div>
-    <div class="container">
+    <div class="container py-5">
         <div class="row">
             <div class="col-6">
                 <div class="content">
-                    <h2>{{ $dett_com[$num]['title'] }}</h2>
+                    <h2 class="text_da">{{ $dett_com[$num]['title'] }}</h2>
                     <div class="d-flex price_av justify-content-between">
                     <div>
                         <div class="d-flex bg_gr p-2 av justify-content-between">
@@ -29,30 +29,57 @@
         
             <div class="col-6">
                 <div class="adv">
-                    <h2>ADVERTISEMENT</h2>
-                    <img src="{{asset('img/adv.jpg')}}" alt="adv">
+                    <h2 class="adv_title">ADVERTISEMENT</h2>
+                    <img src="{{asset('img/adv.jpg')}}" alt="adv">    
                 </div>
             </div>
         </div>
     </div>
-    <div class="bg_g">
+    <div class="bg_g bt_1g">
         <div class="container">
             <div class="row">
-                <div class="col-6">
+                <div class="col-6 py-3">
                     <div class="talent">
-                        <h2>Talent</h2>
-                        <div class="art">
-                            <p>Art by:</p>
-
+                        <h2 class="text_da">Talent</h2>
+                        <div class="art d-flex bt_1g py-2 justify-content-between align-items-center">
+                            <p class='m-0 text_da'>Art by:</p>
+                            <p class="text-primary w_435r m-0">
+                                @foreach($dett_com[$num]['artists'] as $artist)
+                               {{$artist}}
+                                @if(!$loop->last)
+                                 <span class="text-dark">,</span>
+                                @endif
+                                @endforeach
+                            </p>
                         </div>
-                        <div class="writter">
-
+                        <div class="writter d-flex bt_1g bb_1g py-2 justify-content-between align-items-center">
+                        <p class='m-0 text_da'>Written by:</p>
+                            <p class="text-primary w_435r m-0">
+                                @foreach($dett_com[$num]['writers'] as $writers)
+                               {{$writers}}
+                                @if(!$loop->last)
+                                <span class="text-dark">,</span>
+                                @endif
+                                @endforeach
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div class="col-6">
+                <div class="col-6 py-3">
                     <div class="specs">
-                        <h2>Specs</h2>
+                        <h2 class="text_da">Specs</h2>
+                        <div class="series d-flex bt_1g align-items-center justify-content-between py-2">
+                            <p class='m-0 text_da'>Series:</p>
+                            <p class='w_435 m-0 text-primary text-uppercase'>{{ $dett_com[$num]['series'] }}</p> 
+                        </div>
+                        <div class="price d-flex bt_1g align-items-center justify-content-between py-2">
+                            <p class='m-0 text_da'>U.S. Price:</p>
+                            <p class='w_435 m-0 '>{{ $dett_com[$num]['price'] }}</p>
+                        </div>
+                        <div class="sale d-flex bt_1g bb_1g align-items-center justify-content-between py-2">
+                            <p class='m-0 text_da'>On Sale Date:</p>
+                            <p class='w_435 m-0'>{{ $date_format }}</p>
+                        </div>
                     </div>
                 </div>
             </div>
